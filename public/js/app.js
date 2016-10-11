d3fb80673b5b15e8dd8ca5b8b2f05b299f322c98
Harryson Pointdujour <Harryson Pointdujour>
2016-10-10 20:11:36 -0400

    added the ability for user inputs to change grid size

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,28 @@
 // use iife and hit tabe to generate the "function bundle to start"
 (function() {
 
-  // var numberOfRows = 15; //number of rows in grid
-  // var numberOfCols = 15; // number of columns
   var canvas = $('#canvas'); // my placement area - think of paper in drawing
+  var updateGridButton = $('#update-grid-button');
+  var numberOfColsInput = $('#number-of-cols');
+  var numberofRowsInput = $('#number-of-rows');
 
   makeGrid(15,15); //putting rows and cols as input parameters makes it so they have to be passed in
-  clearGrid();
-  makeGrid(30,30);
-    $('.cell').on('click', changeColor);
+  $('.cell').on('click', changeColor);
+  updateGridButton.on('click', updateGridSize);
+  // clearGrid();
+  // makeGrid(30,30);
 
+
+  function updateGridSize(){
+    clearGrid(); //remove current grid
+    //grab number of columns from the input for the new grid
+    var newColNumber = parseInt(numberOfColsInput.val());
+    //grab number rows from the input for the new grid
+    var newRowNumber = parseInt(numberofRowsInput.val());
+    //make new grid based on new rows and columns values
+    makeGrid(newRowNumber , newColNumber);
+    $('.cell').on('click', changeColor);
+  }
   function clearGrid(){
     canvas.empty();
   }
